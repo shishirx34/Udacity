b571aba731b6d1dd6440b9bb5661c7c83a0a1972
Shishir H <shishir.f22@gmail.com>
2018-01-03 21:16:25 -0800

    utils and skeleton

---
+++ naive-bayes-spam-detector/src/spam-filter.py
@@ -0,0 +1,15 @@
+# Main file to read data and define classifier
+
+import pandas as pd
+
+df = pd.read_table('../dataset/smsspamcollection/SMSSpamCollection',
+    sep = '\t',
+    header = None,
+    names = ['label', 'message'])
+
+#Map labels to binary values
+df['label'] = df.label.map({'ham': 0, 'spam': 1})
+
+#Print out first 5 rows
+print(df.shape)
+print(df.head())
---
+++ naive-bayes-spam-detector/utils/bag-of-words.py
@@ -0,0 +1,35 @@
+# Vectorizer for Bag of Words
+import string
+import pprint
+from collections import Counter
+from sklearn.feature_extraction.text import CountVectorizer
+import pandas as pd
+
+count_vector = CountVectorizer()
+
+documents = ['Hello, how are you!',
+             'Win money, win from home.',
+             'Call me now.',
+             'Hello, Call hello you tomorrow?']
+
+# Build the token words
+token_words = []
+for index in documents:
+    token_words.append(index.lower().translate(string.maketrans('', ''), string.punctuation).split(' '))
+
+print token_words
+
+# Build the counter of the token words
+frequency_list = []
+for i in token_words:
+    frequency_counts = Counter(i)
+    frequency_list.append(frequency_counts)
+
+pprint.pprint(frequency_list)
+
+# Using scikit-learn library
+count_vector.fit(documents)
+docarray = count_vector.transform(documents).toarray()
+
+frequency_matrix = pd.Dataframe(docarray, columns = count_vector.get_feature_names())
+print frequency_matrix
