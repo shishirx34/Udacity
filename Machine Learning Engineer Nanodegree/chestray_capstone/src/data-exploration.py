3d680f2dedd8660efc12eed7dcde1527994a03d9
shishirx34 <shishir.f22@gmail.com>
2019-02-02 09:46:59 +0000

    data exploration

--- Machine Learning Engineer Nanodegree/chestray_capstone/src/data-exploration.py
+++ Machine Learning Engineer Nanodegree/chestray_capstone/src/data-exploration.py
@@ -1,8 +1,41 @@
 # Feature exploration for ChestR
 
-# Load Configs
+import numpy as np 
+import pandas as pd 
+import os 
 from Configurations.Config import Config
 
+# Load Configs
 config = Config.GetConfig()
 
-print (config.DataFolderPath)
+# Load Data files
+data_file = os.path.join(config.DataFolderPath, "Data_Entry_2017.csv")
+df = pd.read_csv(data_file)
+
+# Data load check
+print(df.head())
+
+#Explore input data features
+print("\nTotal #XRay Reports:", df.shape[0])
+
+#Get all disease labels
+disease_labels = df['Finding Labels'].str.split("|", n=None, expand=True).stack().reset_index()[0].unique()
+print("Total #Finding labels in dataset:", disease_labels.shape[0])
+
+#Plot Finding Labels distribution
+
+#Plot Gender distribution
+print("\nGenerating XRay Reports vs Patient Gender distribution plot...") 
+print(df['Patient Gender'].value_counts())
+
+#Plot patient age distribution
+print("\nGenerating Xray reports vs Patient Age distribution plot...")
+
+#Plot Labels vs Gender distribution
+print("\nGenerating XRay report Labels vs Patient Gender distribution plot...") 
+
+#Plot Labels vs Age
+print("\nGenerating Xray report Labels vs Patient Age distribution plot...")
+
+#Plot MultiLabel relations
+print("\nGenerating multi label co-relation plot...")
